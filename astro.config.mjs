e1dd8a9d0bdbc7a6c74decef542c772753377b3c
Glowin <jiangbian66@gmail.com>
2024-05-06 01:31:54 +0800

    setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,16 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: cloudflare()
+	output: "server",
+	adapter: cloudflare({
+		runtime: {
+			mode: "local",
+			type: "pages",
+			bindings: {
+				DB: {
+					type: "d1",
+				},
+			},
+		},
+	}),
 });
