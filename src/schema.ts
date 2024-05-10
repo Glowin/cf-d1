dc63bae43f3ff561a527c3b307d792b89f27cdb2
Glowin <jiangbian66@gmail.com>
2024-05-10 16:14:44 +0800

    modify code

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,10 @@
 import { drizzle } from "drizzle-orm/d1";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+export const getDb = (locals: App.Locals) => {
+    return drizzle(locals.runtime.env.DB);
+}; 
+
 export const linkShare = sqliteTable("linkShare", {
 	id: integer("id").primaryKey({
 		autoIncrement: true,
